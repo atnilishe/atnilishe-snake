cbf86ed4647d100b9751f0a1f77c395d75f57496
atnilishe <atnilishe@gmail.com>
2017-11-12 15:09:00 +0200

    main menu

--- server.js
+++ server.js
@@ -28,63 +28,71 @@ io.use(sharedsession(session, {
 app.set('port', port);
 app.use('/client', express.static(__dirname + '/client'));
 
-app.get('/favicon.ico', function (req, response) {
+app.get('/favicon.ico', function(req, response) {
     response.sendFile(path.join(__dirname, 'favicon.ico'));
 });
 
 // Маршруты
-app.get('/', function (req, response) {
+app.get('/', function(req, response) {
     response.sendFile(path.join(__dirname, './client/views/index.html'));
 });
 
-app.get('/auth', function (req, response) {
+app.get('/auth', function(req, response) {
     response.sendFile(path.join(__dirname, './client/views/auth.html'));
 });
 
-app.get('/fonts', function (req, response) {
+app.get('/fonts', function(req, response) {
     response.sendFile(path.join(__dirname, './client/views/fonts.html'));
 });
 
-app.get('/remote', function (req, res) {
+app.get('/remote', function(req, res) {
     res.json(req.session.userData.uid);
 });
 
-app.get('/remote/:key', function (req, response) {
+app.get('/remote/:key', function(req, response) {
     req.session.userData = { uid: req.params.key };
     req.session.save();
     response.sendFile(path.join(__dirname, './client/views/controller.html'));
 });
 
-app.get('/game', function (req, response) {
+app.get('/game', function(req, response) {
     response.sendFile(path.join(__dirname, './client/views/game.html'));
 });
 
-app.post('/game', function (req, response) {
-    var nickname = req.body.nickname;
-    req.session.userData = { uid: nickname, first_name: nickname, last_name: '' };
-    req.session.save();
-    response.writeHead(302, { 'Location': '/game' });
-    response.end();
+app.get('/main_menu', function(req, response) {
+    response.sendFile(path.join(__dirname, './client/views/main_menu.html'));
 });
 
-app.post('/client/views/players', function (req, res) {
-    var url = 'http://ulogin.ru/token.php?token=' + req.body.token + '&host=' + 'localhost';
-    var response = res;
-    http.get(url, (res) => {
-        res.on('data', (d) => {
-            // TODO: create user data
-            req.session.userData = JSON.parse(new Buffer(d).toString());
-            req.session.save();
-            response.writeHead(302, { 'Location': '/game' });
-            response.end();
+app.get('/battle', function(req, response) {
+    response.sendFile(path.join(__dirname, './client/views/battle.html'));
+});
+
+app.post('/players', function(req, res) {
+    if (req.body.nickname) {
+        var nickname = req.body.nickname;
+        req.session.userData = { uid: nickname, first_name: nickname, last_name: '' };
+        req.session.save();
+        res.writeHead(302, { 'Location': '/main_menu' });
+        res.end();
+    } else {
+        var url = 'http://ulogin.ru/token.php?token=' + req.body.token + '&host=' + 'localhost';
+        var response = res;
+        http.get(url, (res) => {
+            res.on('data', (d) => {
+                // TODO: create user data
+                req.session.userData = JSON.parse(new Buffer(d).toString());
+                req.session.save();
+                response.writeHead(302, { 'Location': '/main_menu' });
+                response.end();
+            });
+        }).on('error', (e) => {
+            console.error(e);
         });
-    }).on('error', (e) => {
-        console.error(e);
-    });
+    }
 });
 
 // Запуск сервера
-server.listen(port, function () {
+server.listen(port, function() {
     console.log('Listening on ' + port);
 });
 
@@ -92,32 +100,32 @@ var Game = require("./server/game.js");
 var game_angine = new Game();
 game_angine.addFood();
 
-setInterval(function () {
+setInterval(function() {
     game_angine.makeMove();
     io.sockets.emit('state', game_angine.getGameState());
 }, 200);
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
     // TODO: не пускать уже играющих игроков
     var user = socket.handshake.session.userData;
     if (user == undefined) {
         // TODO : redirect to / for auth
         socket.disconnect();
     }
-    socket.on('new player', function () {
+    socket.on('new player', function() {
         try {
             var user = socket.handshake.session.userData;
             player = {};
             player.name = user.first_name + ' ' + user.last_name;
             player.id = user.uid;
-            game_angine.addPlayer(player.id, player, function () {
+            game_angine.addPlayer(player.id, player, function() {
                 io.sockets.emit('state', game_angine.getGameState());
             });
         } catch (error) {
             console.log('Error on new player' + error.name + ":" + error.message + "\n" + error.stack);
         }
     });
-    socket.on('movement', function (data) {
+    socket.on('movement', function(data) {
         try {
             var user = socket.handshake.session.userData;
             player.id = user.uid;
@@ -126,7 +134,7 @@ io.on('connection', function (socket) {
             console.log('Error on movement' + error.name + ":" + error.message + "\n" + error.stack);
         }
     });
-    socket.on('disconnect', function () {
+    socket.on('disconnect', function() {
         try {
             var user = socket.handshake.session.userData;
             player.id = user.uid;
